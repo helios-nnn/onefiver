bb4462360de04cdfdcb3b57d142d68bce93393b8
helios-nnn <nunkeshave@gmail.com>
2026-04-26 22:31:34 +0400

    new website uploaded

--- fix.js
+++ fix.js
@@ -1,15 +1,6 @@
 const fs = require('fs');
-
-// Read original file
 let html = fs.readFileSync('index.html', 'utf8');
-
-// Replace ALL ../assets/ → assets/
 html = html.replace(/\.\.\/assets\//g, 'assets/');
-
-// Replace any remaining ../ (safety cleanup)
 html = html.replace(/\.\.\//g, '');
-
-// Save new file
 fs.writeFileSync('index-fixed.html', html);
-
-console.log('✅ index-fixed.html created');
+console.log('index-fixed.html created');
